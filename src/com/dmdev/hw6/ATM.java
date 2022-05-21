c90f3ed52eba43d9e554a1c43eb776e225e3e424
Vyacheslav <blagoff@me.com>
2022-05-21 15:31:53 +0300

    hw6

---
+++ src/com/dmdev/hw6/ATM.java
@@ -0,0 +1,111 @@
+package com.dmdev.hw6;
+
+public class ATM {
+
+    private final int TWENTY = 20;
+    private final int FIFTY = 50;
+    private final int ONE_HUNGRED = 100;
+
+    int[] Banknotes = {ONE_HUNGRED, FIFTY, TWENTY};
+
+    private int twentyNominationQuantity;
+    private int fiftyNominationQuantity;
+    private int oneHundredNominationQuantity;
+
+    public void setTwentyNominationQuantity(int twentyNominationQuantity) {
+        this.twentyNominationQuantity = twentyNominationQuantity;
+    }
+
+    public void setFiftyNominationQuantity(int fiftyNominationQuantity) {
+        this.fiftyNominationQuantity = fiftyNominationQuantity;
+    }
+
+    public void setOneHundredNominationQuantity(int oneHundredNominationQuantity) {
+        this.oneHundredNominationQuantity = oneHundredNominationQuantity;
+    }
+
+
+    public int getTwentyNominationQuantity() {
+        return twentyNominationQuantity;
+    }
+
+    public int getFiftyNominationQuantity() {
+        return fiftyNominationQuantity;
+    }
+
+
+    public int getOneHundredNominationQuantity() {
+        return oneHundredNominationQuantity;
+    }
+
+
+    public ATM(int twentyNominationQuantity, int fiftyNominationQuantity, int oneHundredNominationQuantity) {
+        this.twentyNominationQuantity = twentyNominationQuantity;
+        this.fiftyNominationQuantity = fiftyNominationQuantity;
+        this.oneHundredNominationQuantity = oneHundredNominationQuantity;
+    }
+
+
+    public void putMoney(int inputTwentyNominationQuantity, int inputFiftyNominationQuantity,
+                         int inputOneHundredNominationQuantity) {
+        this.setTwentyNominationQuantity(this.getTwentyNominationQuantity() + inputTwentyNominationQuantity);
+        this.setFiftyNominationQuantity(this.getFiftyNominationQuantity() + inputFiftyNominationQuantity);
+        this.setOneHundredNominationQuantity(this.getOneHundredNominationQuantity() + inputOneHundredNominationQuantity);
+    }
+
+
+    private int getNominationQuantity(int nomination) {
+        return switch (nomination) {
+            case 20 -> this.getTwentyNominationQuantity();
+            case 50 -> this.getFiftyNominationQuantity();
+            case 100 -> this.getOneHundredNominationQuantity();
+            default -> 0;
+        };
+    }
+
+    private void setNominationQuantity(int nomination, int value) {
+        switch (nomination) {
+            case 20 -> this.setTwentyNominationQuantity(value);
+            case 50 -> this.setFiftyNominationQuantity(value);
+            case 100 -> this.setOneHundredNominationQuantity(value);
+            default -> {
+            }
+        }
+        ;
+    }
+
+    private int getCurrentSum() {
+        return this.getTwentyNominationQuantity() * 20 + this.getFiftyNominationQuantity() * 50 + this.getOneHundredNominationQuantity() * 100;
+    }
+
+    public boolean withdrawMoney(int withdrawSum) {
+        int[] withdrawNominationAmount = new int[Banknotes.length];
+        int withdrawBalance = withdrawSum;
+        int currentSum = this.getCurrentSum();
+        if (withdrawBalance > currentSum) {
+            return false;
+        }
+        for (int i = 0; i < Banknotes.length; i++) {
+            int withdrawNominationAmountNeeded = withdrawBalance / Banknotes[i];
+            int quantity = this.getNominationQuantity(Banknotes[i]);
+            if (quantity < withdrawNominationAmountNeeded) {
+                withdrawNominationAmountNeeded = quantity;
+            }
+            withdrawNominationAmount[i] = withdrawNominationAmountNeeded;
+            withdrawBalance -= withdrawNominationAmountNeeded * Banknotes[i];
+            if (withdrawBalance == 0) {
+                break;
+            }
+        }
+        if (withdrawBalance != 0) {
+            return false;
+        }
+        for (int i = 0; i < Banknotes.length; i++) {
+            int quantity = this.getNominationQuantity(Banknotes[i]);
+            System.out.println("Выдается " + withdrawNominationAmount[i] + " купюры номиналом " + Banknotes[i]);
+            this.setNominationQuantity(Banknotes[i], quantity - withdrawNominationAmount[i]);
+        }
+        return true;
+
+    }
+}
